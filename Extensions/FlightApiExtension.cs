b9ce85b272b067686ee33de484955d4d7dfbe121
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-07-03 19:19:55 -0300

    styles to edit form

--- Extensions/FlightApiExtension.cs
+++ Extensions/FlightApiExtension.cs
@@ -31,7 +31,20 @@ namespace IntegrationProject.Extensions
                 await _context.SaveChangesAsync();
                 return Results.NoContent();
             });
+            builder.MapPut("flights/{id}", async (ApplicationDbContext _context, int id) =>
+            {
+                if (id <= 0)
+                    return Results.BadRequest();
 
+                var flight = await _context.Flight.SingleOrDefaultAsync(x => x.Id == id);
+
+                if (flight == null)
+                    return Results.NotFound();
+
+                _context.Flight.Remove(flight);
+                await _context.SaveChangesAsync();
+                return Results.NoContent();
+            });
 
 
             return builder;
